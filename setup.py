bcc356a6389a822c9ccd49e979d8bcf365b3a5d8
verybada <lon_bad@hotmail.com>
2017-05-21 17:37:50 +0800

    setup.py: modify url and keywords

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ from setuptools import setup
 setup(
     name="RarbgAPI",
     version="0.1",
-    author="Lin Chi Lung",
+    author="verybada",
     author_email="verybada.lin@gmail.com",
     description=("A simple interface of RARBG.to"),
     license="BSD",
-    keywords="rarbg api",
+    keywords=['rarbg', 'rarbg-torrentapi', 'api', 'python'],
     packages=['rarbgapi'],
     install_requires=[
         'requests'
     ],
     extras_require={
         'travis': ['pep8', 'pylint']
-    }
+    },
+    url='https://github.com/verybada/rarbgapi/'
 )
