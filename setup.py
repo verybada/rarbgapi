507db08c29da4376e1783901908dd493015748c5
verybada <lon_bad@hotmail.com>
2018-07-14 18:03:21 +0800

    setup: fix the version

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
+import os
 from setuptools import setup
-from rarbgapi.__version__ import __version__
 
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = dict()
+with open(os.path.join(here, 'rarbgapi', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
 
 setup(
-    name="RarbgAPI",
-    version=__version__,
+    name='RarbgAPI',
+    version=about['__version__'],
     author="verybada",
     author_email="verybada.lin@gmail.com",
     description=("A simple interface of RARBG.to"),
