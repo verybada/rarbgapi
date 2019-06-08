973755ed11da133f50501e1fe8cb7aa7f5cccf26
verybada <lon_bad@hotmail.com>
2019-06-08 13:36:32 +0800

    setup.py: Update project description

--- setup.py
+++ setup.py
@@ -7,12 +7,18 @@ about = dict()
 with open(os.path.join(here, 'rarbgapi', '__version__.py'), 'r') as f:
     exec(f.read(), about)
 
+long_description = None
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='RarbgAPI',
     version=about['__version__'],
     author="verybada",
     author_email="verybada.lin@gmail.com",
-    description=("A simple interface of RARBG.to"),
+    description=("A python3 wrapper for RARBG.to"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="BSD",
     keywords=['rarbg', 'rarbg-torrentapi', 'api', 'python'],
     packages=['rarbgapi'],
@@ -25,5 +31,10 @@ setup(
     url='https://github.com/verybada/rarbgapi/',
     entry_points={
         'console_scripts': ['rarbgapi=rarbgapi.__main__:main']
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
 )
